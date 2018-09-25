a113230411758d46fa8c8970a8d9b59d743cf408
Gabriel Laet <gabriel.laet@gmail.com>
2018-09-25 19:43:07 -0300

    Adding OpenGraph high-res image

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -4,6 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './header'
+import opengraphImageHigh from '../../images/opengraph-high.png';
 import opengraphImage from '../../images/opengraph.png';
 import opengraphImageLow from '../../images/opengraph-low.png';
 import './layout.css'
@@ -27,7 +28,7 @@ const Layout = ({ children, internal, title }) => (
           meta={[
             { name: 'description', content: 'A EmCasa nasceu para transformar a experiência de comprar e vender um imóvel no Brasil. Nós usamos tecnologia, inteligência de dados e um modelo de negócio inovador para redefinir o mercado brasileiro de Real Estate em favor dos compradores e vendedores.' },
             { name: 'keywords', content: 'emcasa, startup, real estate tech' },
-            { name: 'og:image', content: opengraphImage },
+            { name: 'og:image', content: opengraphImageHigh },
             { name: 'og:image', content: opengraphImageLow },
             { name: 'image', property: 'og:image', content: opengraphImage }
           ]}
